29d24fd16ec1e6426955e7f1864fdf89fa15e4ac
Chabi Amine sif eddine <chabiaminesifeddine@gmail.com>
2025-08-05 12:57:09 +0100

    feat (project files) : add initial project files

---
+++ userProvisioning/src/main/java/com/chabiaminesifeddine/userProvisioning/UserProvisioningApplication.java
@@ -0,0 +1,13 @@
+package com.chabiaminesifeddine.userProvisioning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProvisioningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProvisioningApplication.class, args);
+	}
+
+}
---
+++ userProvisioning/src/test/java/com/chabiaminesifeddine/userProvisioning/UserProvisioningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chabiaminesifeddine.userProvisioning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProvisioningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
